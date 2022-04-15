e92f016b6df43425f0e9abc824e67c8a238cbfb4
unknown <patryk_pora@o2.pl>
2022-04-15 18:51:57 +0200

    class and controller

---
+++ src/main/java/pl/patrykpora/restinaction/Message.java
@@ -0,0 +1,13 @@
+package pl.patrykpora.restinaction;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+
+    private String text;
+}
