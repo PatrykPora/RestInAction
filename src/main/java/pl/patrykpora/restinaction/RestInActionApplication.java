5b4910809dc4d1854b67329b38f6828582e0bda3
unknown <patryk_pora@o2.pl>
2022-04-15 18:40:15 +0200

    initial

---
+++ src/main/java/pl/patrykpora/restinaction/RestInActionApplication.java
@@ -0,0 +1,13 @@
+package pl.patrykpora.restinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestInActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/patrykpora/restinaction/RestInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.patrykpora.restinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
